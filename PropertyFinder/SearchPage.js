aac77c6c9165ed59a36a4fd24a961e40301ba02a
Emmet Susslin <esusslin@gmail.com>
2017-07-14 11:07:34 -0700

    lol?

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -11,6 +11,10 @@ import {
   Image
 } from 'react-native';
 
+var SearchResults = require('./SearchResults');
+
+
+
 function urlForQueryAndPage(key, value, pageNumber) {
   var data = {
       country: 'uk',
@@ -64,7 +68,11 @@ class SearchPage extends Component {
   _handleResponse(response) {
   this.setState({ isLoading: false , message: '' });
   if (response.application_response_code.substr(0, 1) === '1') {
-    console.log('Properties found: ' + response.listings.length);
+    this.props.navigator.push({
+	  title: 'Results',
+	  component: SearchResults,
+	  passProps: {listings: response.listings}
+	});
   } else {
     this.setState({ message: 'Location not recognized; please try again.'});
   }
